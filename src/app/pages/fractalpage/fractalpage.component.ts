37b75296177f30da75c934ccb8c5dc75fdd744d9
Valeriy Inshyn <valeriyi.nshyn@intenselab.com>
2023-10-23 13:48:18 +0300

    feat: iterations input

--- src/app/pages/fractalpage/fractalpage.component.ts
+++ src/app/pages/fractalpage/fractalpage.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './fractalpage.component.html',
   styleUrls: ['./fractalpage.component.scss']
 })
-export class FractalpageComponent {
 
+export class FractalpageComponent {
+  iteration: number = 4;
+
+  drawFractal(iterationValue: string): void {
+    // Parse the input value as an integer
+    this.iteration = parseInt(iterationValue, 10);
+
+    // Check if parsing was successful and it's a valid integer
+    if (!isNaN( this.iteration)) {
+      // Use the 'iteration' variable as an integer
+      console.log('Integer Value:',  this.iteration);
+
+      // You can perform further actions with 'iteration'
+      // For example, call a function that uses 'iteration' for drawing a fractal.
+    } else {
+      // Handle the case where the input is not a valid integer
+      console.error('Invalid Integer Value');
+    }
+  }
 }
