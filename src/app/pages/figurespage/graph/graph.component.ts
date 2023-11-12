3bbf2c04fcd6797e8a023c5535b74cfba04af5e1
Volodymyr Domin <mindotale@gmail.com>
2023-11-12 17:33:59 +0200

    feat: add parallelogram and line drawing

--- src/app/pages/figurespage/graph/graph.component.ts
+++ src/app/pages/figurespage/graph/graph.component.ts
@@ -16,12 +16,14 @@ export class GraphComponent implements OnInit, AfterViewInit, OnChanges, OnDestr
   @Input() b: number = 0;
 
   private p5Instance!: p5;
+  private container!: any;
 
   constructor() { }
 
-  ngOnInit() {  }
+  ngOnInit() { }
 
   ngAfterViewInit() {
+    this.container = this.graphContainer.nativeElement;
     this.createCanvas();
     this.handleResize();
   }
@@ -35,62 +37,104 @@ export class GraphComponent implements OnInit, AfterViewInit, OnChanges, OnDestr
   }
 
   private createCanvas() {
-    const container = this.graphContainer.nativeElement;
-    this.p5Instance = new p5(this.sketch, container);
+    this.p5Instance = new p5(this.sketch, this.container);
   }
 
   private sketch = (p: p5) => {
     p.setup = () => {
-      const container = this.graphContainer.nativeElement;
-      p.createCanvas(container.offsetWidth, container.offsetWidth);
+      p.createCanvas(this.container.offsetWidth, this.container.offsetWidth);
       p.noLoop();
     };
 
     p.draw = () => {
+      p.background(255);
+
+      // Draw axes
+      drawAxes(p);
+
+      // Draw line
+      drawLine(p);
+
+      // Draw points
+      drawPoints(p, this.p1, this.p2, this.p3);
+
+      // Draw parallelogram
+      drawParallelogram(p, this.p1, this.p2, this.p3);
+
       console.log('p1:', this.p1);
       console.log('p2:', this.p2);
       console.log('p3:', this.p3);
       console.log('a:', this.a);
       console.log('b:', this.b);
-      p.background(255);
-
-      // Draw the original parallelogram
-      p.fill(150, 150, 150);
-      p.quad(this.p1.x, this.p1.y, this.p2.x, this.p2.y, this.p3.x, this.p3.y, this.p1.x + this.p2.x - this.p3.x, this.p1.y + this.p2.y - this.p3.y);
-
-      // Draw the line
-      p.stroke(0);
-      p.line(0, this.b, p.width, this.a * p.width + this.b);
-
-      // Reflect the points
-      const reflectedP1 = reflectPoint(this.p1);
-      const reflectedP2 = reflectPoint(this.p2);
-      const reflectedP3 = reflectPoint(this.p3);
-
-      // Draw the reflected parallelogram
-      p.fill(100, 100, 255);
-      p.quad(reflectedP1.x, reflectedP1.y, reflectedP2.x, reflectedP2.y, reflectedP3.x, reflectedP3.y, reflectedP1.x + reflectedP2.x - reflectedP3.x, reflectedP1.y + reflectedP2.y - reflectedP3.y);
     };
 
-    const reflectPoint = (point: { x: number, y: number }) => {
-      const xTranslated = point.x;
-      const yTranslated = point.y - this.b / this.a;
+    const drawAxes = (p: p5) => {
+      const centerX = p.width / 2;
+      const centerY = p.height / 2;
 
-      const xReflected = xTranslated;
-      const yReflected = -yTranslated;
+      // Draw X-axis
+      p.stroke(0);
+      p.line(0, centerY, p.width, centerY);
 
-      const xFinal = xReflected;
-      const yFinal = yReflected + this.b / this.a;
+      // Draw Y-axis
+      p.line(centerX, 0, centerX, p.height);
+    };
 
-      return { x: xFinal, y: yFinal };
+    const drawPoints = (p: p5, ...points: { x: number, y: number }[]) => {
+      const centerX = p.width / 2;
+      const centerY = p.height / 2;
+
+      p.stroke(0, 0, 255); // Blue color for points
+      p.strokeWeight(5); // Adjust the size of the points
+
+      points.forEach(point => {
+        p.point(centerX + point.x, centerY - point.y);
+      });
+    };
+
+    const drawParallelogram = (p: p5, p1: { x: number, y: number }, p2: { x: number, y: number }, p3: { x: number, y: number }) => {
+      const centerX = p.width / 2;
+      const centerY = p.height / 2;
+
+      // Draw the parallelogram
+      const p4 = calculateFourthPoint(p1, p2, p3);
+      p.line(centerX + p1.x, centerY - p1.y, centerX + p2.x, centerY - p2.y);
+      p.line(centerX + p2.x, centerY - p2.y, centerX + p3.x, centerY - p3.y);
+      p.line(centerX + p3.x, centerY - p3.y, centerX + p4.x, centerY - p4.y);
+      p.line(centerX + p4.x, centerY - p4.y, centerX + p1.x, centerY - p1.y);
+    };
+
+    const drawLine = (p: p5) => {
+      const centerX = p.width / 2;
+      const centerY = p.height / 2;
+
+      // Draw the line y = ax + b
+      p.stroke(255, 0, 0); // Red color for the line
+      p.strokeWeight(3); // Adjust the weight of the line
+
+      // Define the range of x values for the line
+      const startX = -p.width / 2;
+      const endX = p.width / 2;
+
+      // Draw the line segment
+      p.line(centerX + startX, centerY - (this.a * startX + this.b), centerX + endX, centerY - (this.a * endX + this.b));
+    };
+
+    const calculateFourthPoint = (p1: { x: number, y: number }, p2: { x: number, y: number }, p3: { x: number, y: number }) => {
+      // Calculate the fourth point based on the properties of a parallelogram
+      const p4x = p1.x + p3.x - p2.x;
+      const p4y = p1.y + p3.y - p2.y;
+      return { x: p4x, y: p4y };
     };
   };
 
   @HostListener('window:resize')
-  private handleResize() {
-    if (this.p5Instance) {
-      const container = this.graphContainer.nativeElement;
-      this.p5Instance.resizeCanvas(container.offsetWidth, container.offsetWidth);
-    }
-  }
+    private handleResize() {
+      if (this.p5Instance) {
+        const container = this.graphContainer.nativeElement;
+        this.p5Instance.resizeCanvas(container.offsetWidth, container.offsetWidth);
+      }
+    };
+
+   
 }
